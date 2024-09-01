2a55b5c538a29637ac17bb715d226c5ea382c69d
sejongPark <lifedesigner88@gmail.com>
2024-09-01 16:41:10 +0900

    10811

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
+import java.io.*;
 
 public class Main {
     public static void main(String args[]) throws IOException {
@@ -10,13 +6,38 @@ public class Main {
         InputStreamReader isr = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(isr);
 
-        Set<Integer> set = new HashSet<>();
+        OutputStreamWriter osw = new OutputStreamWriter(System.out);
+        BufferedWriter bw = new BufferedWriter(osw);
 
-        for (int i = 0; i < 10; i++) {
-            int num = Integer.parseInt(br.readLine());
-            set.add(num % 42);
+        String str = br.readLine();
+        int bucketsNumber = Integer.parseInt(str.charAt(0) + "");
+        int counter = Integer.parseInt(str.charAt(2) + "");
+
+        int[] buckets = new int[bucketsNumber + 1];
+
+        for (int i = 1; i <= bucketsNumber; i++) buckets[i] = i;
+        for (int i = 0; i < counter; i++) {
+            String range = br.readLine();
+            int start = Integer.parseInt(range.charAt(0) + "");
+            int end = Integer.parseInt(range.charAt(2) + "");
+            changeArrayWithRange(buckets, start, end);
         }
-        System.out.println(set.size());
+
+        for (int i = 1; i <= bucketsNumber; i++)
+            bw.write(buckets[i] + " ");
+
+        bw.flush();
+        bw.close();
+
+
+    }
+
+    static void changeArrayWithRange(int[] arr, int start, int end) {
+        for (int i = 0; i <= (end - start) / 2; i++) {
+            int temp = arr[start + i];
+            arr[start + i] = arr[end - i];
+            arr[end - i] = temp;
+        }
     }
 
 }
